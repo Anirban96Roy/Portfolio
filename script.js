6fa094b4a79d896d76c62c9d0d8387a98be48a79
anirban <roy778081@gmail.com>
2024-02-10 23:09:04 +0600

    homepage without responsive

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".multiple",{
+    strings:["Student","Debater","Programmer","Singer","Devoloper"],
+    typeSpeed:100,
+    backspeed:100,
+    backdelay:1000,
+    loop: true
+})
